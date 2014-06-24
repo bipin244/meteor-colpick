f4ac054a08abf21a3451df40147c714eba21a947
tommyJS <tomaszadrianowski@gmail.com>
2014-06-24 17:18:59 +0200

    meteor-colpick

--- package.js
+++ package.js
@@ -1,8 +1,10 @@
 Package.describe({
-    summary: "Lightweight jQuery Color Picker plugin with RGB, HSB and HEX fields, several skins and layouts."
+    summary: "Lightweight jQuery Color Picker plugin with RGB, HSB and HEX fields, several skins and layouts - packaged for meteor."
 });
 
 Package.on_use(function (api) {
+    api.use('jquery', 'client');
+
     api.add_files([
         "lib/bootstrap-colorpicker/js/bootstrap-colorpicker.js",
         "lib/bootstrap-colorpicker/css/bootstrap-colorpicker.css",
