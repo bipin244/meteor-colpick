1672af058f652ec412dea326c16e89d1802ec3ea
tommyJS <tomaszadrianowski@gmail.com>
2014-06-24 17:25:43 +0200

    meteor-colpick

--- package.js
+++ package.js
@@ -6,11 +6,8 @@ Package.on_use(function (api) {
     api.use('jquery', 'client');
 
     api.add_files([
-        "lib/bootstrap-colorpicker/js/bootstrap-colorpicker.js",
-        "lib/bootstrap-colorpicker/css/bootstrap-colorpicker.css",
-        "lib/bootstrap-colorpicker/img/bootstrap-colorpicker/alpha.png",
-        "lib/bootstrap-colorpicker/img/bootstrap-colorpicker/hue.png",
-        "lib/bootstrap-colorpicker/img/bootstrap-colorpicker/saturation.png"
+        "lib/css/colpick.css",
+        "lib/js/colpick.js"
     ], "client");
 
     api.add_files('path-override.css', 'client');
